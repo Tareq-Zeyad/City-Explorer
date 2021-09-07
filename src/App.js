0459148a9a6d02ebd041dd0b4fa29b61e688ec3b
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-07 22:14:51 +0300

    lab07 complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Weather from './components/Weather';
 
 
 class App extends React.Component {
@@ -17,6 +17,8 @@ class App extends React.Component {
       lat: '',
       lon: '',
       displayName: '',
+      date: '',
+      description:'',
       mapFlag: false,
       displayErr: false
     }
@@ -27,11 +29,12 @@ class App extends React.Component {
 
     event.preventDefault();
     let cityName = event.target.cityName.value;
-    // let keyAPI = 'pk.ecabd378ab96c0605c0417b2d7af1979';
     // let keyAPI = process.env.REACT_KEY;
-    console.log(cityName);
-    console.log(process.env.REACT_APP_LOCATION_IQ_KEY);
+    // console.log(process.env.REACT_APP_LOCATION_IQ_KEY);
     let URl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${cityName}&format=json`;
+
+    let weatherURL = await axios.get(`http://localhost:3010/weather?searchQuery=${cityName}`)
+    console.log(weatherURL.data);
 
     try {
       // axious : to send a request from the client side (browser) to the API server.
@@ -42,6 +45,9 @@ class App extends React.Component {
         lat: response.data[0].lat,
         lon: response.data[0].lon,
         displayName: response.data[0].display_name,
+        date: weatherURL.data[0].date,
+        description: weatherURL.data[0].description,
+
         mapFlag: true,
       })
     }
@@ -54,22 +60,27 @@ class App extends React.Component {
     }
   }
 
+
   render() {
     return (
       <>
-        <Header className="Header"/>
+        <Header className="Header" />
         <Form className="Form" onSubmit={this.getLocationData}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Enter City</Form.Label>
-        <Form.Control  name='cityName' type="text" placeholder="Enter City Name here" />
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Enter City</Form.Label>
+            <Form.Control name='cityName' type="text" placeholder="Enter City Name here" />
           </Form.Group>
           <Button type='submit'>Explore</Button>
         </Form>
 
         {/* Render the city data */}
-        <p>DisplayName: {this.state.displayName}</p>
+        <p>Display Name: {this.state.displayName}</p>
         <p>Lat : {this.state.lat}</p>
         <p>Lon : {this.state.lon}</p>
+        <p>Date : {this.state.date} </p>
+        <p>Describtion : {this.state.description} </p>
+      
+
 
         {this.state.mapFlag && <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${this.state.lat},${this.state.lon}`} alt='map' />}
 
